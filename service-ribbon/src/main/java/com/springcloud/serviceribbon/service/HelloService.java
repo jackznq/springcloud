465a612a6f240823ae6bdb17e1337eff0895d88b
shzhangnaiquan <shzhangnaiquan@jd.com>
2019-05-13 17:49:40 +0800

    spring cloud 负载均衡

--- service-ribbon/src/main/java/com/springcloud/serviceribbon/service/HelloService.java
+++ service-ribbon/src/main/java/com/springcloud/serviceribbon/service/HelloService.java
@@ -1,5 +1,6 @@
 package com.springcloud.serviceribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -15,8 +16,14 @@ public class HelloService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://eureka-client/hi?" + name, String.class);
 
+    }
+
+    public String hiError(String name) {
+
+        return "sorry,service error"+name;
     }
 }
