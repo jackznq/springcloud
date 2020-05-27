5e1c9d550db97304739805ed71d1f2d2dc16d6fd
jackznq <zhangnaiquan@qjdchina.com>
2020-05-27 11:27:58 +0800

    commit

--- eureka-server/src/main/java/com/znq/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/znq/eurekaserver/EurekaServerApplication.java
@@ -2,10 +2,13 @@ package com.znq.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableDiscoveryClient
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
